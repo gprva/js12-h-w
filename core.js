55ca845bcf767e7f60104aba305650424a847f05
gprva <lolasum12345@gmail.com>
2024-01-17 16:24:59 +0600

    added calculator

--- core.js
+++ core.js
@@ -1,10 +1,29 @@
 const input1 = $('.number-1')
 const input2 = $('.number-2') 
-const btnPlus = $('.btn__plus') 
+const btnPlus = $('.btn__minus') 
 const result = $('.result')
+const btnMinus = $('.btn__plus') 
+const btnDivide = $('.btn__divide') 
+const btnMultiply = $('.btn__multiply') 
+
+
 
 btnPlus.on('click',function(){
     let sum = +input1.val() +  +input2.val()
     result.text(sum)
 })
 
+btnMinus.on('click',function(){
+    let min = +input1.val() -  +input2.val()
+    result.text(min)
+})
+
+btnDivide.on('click',function(){
+    let sum = +input1.val() /  +input2.val()
+    result.text(sum)
+})
+
+btnMultiply.on('click',function(){
+    let sum = +input1.val() *  +input2.val()
+    result.text(sum)
+})
